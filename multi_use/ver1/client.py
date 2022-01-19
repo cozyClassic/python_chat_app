38c0bff6eaab2598a25c59917667153594e53e48
KIM <km0192@naver.com>
2022-01-19 14:27:36 +0900

    feat(multi_use.ver1) : 주석 추가

--- multi_use/ver1/client.py
+++ multi_use/ver1/client.py
@@ -1,19 +1,18 @@
-import _thread
 from socket import *
-
-_thread.start_new_thread()
 
 port = 8080
 
 client_socket = socket(AF_INET, SOCK_STREAM)
 client_socket.connect(('127.0.0.1', port))
 
+# 1. 로컬서버 (127.0.0.1)의 8080포트에 연결한다.
+
 print('클라이언트 접속 완료')
 
+
 while True :
-    received_data = client_socket.recv(1024)
-    print('서버측 메시지 : ', received_data.decode('utf-8'))
+    received_data = client_socket.recv(1024)                # 2. 서버에서 받은 값을 저장해서 print한다.
+    print('서버측 메시지 : ', received_data.decode('utf-8')) 
 
-    send_data = input('>>>')
-    client_socket.send(send_data.encode('utf-8'))
-
+    send_data = input('>>>')                                # 3. '>>>'를 출력 후 값을 저장한다.
+    client_socket.send(send_data.encode('utf-8'))           # 4. 데이터를 서버에 전송한다. 2번부터 반복한다.
--- multi_use/ver1/servrer.py
+++ multi_use/ver1/servrer.py
@@ -1,22 +1,24 @@
-from audioop import add
 from socket import *
 
 port = 8080
 
 server_socket = socket(AF_INET, SOCK_STREAM)
 server_socket.bind(('',port))
-server_socket.listen(1)
+# 1. 접속을 받는 상태로 변경
+server_socket.listen(1)                     
 
 print(f'{port}번 포트로 접속 대기중 ..')
 
+# 2. 누군가가 접속하면 accept()에서 return이 발생한다.
+# 2-1. 이 return에는 연결된 socket이 포함된다.
 connection_socket, address = server_socket.accept()
 
 print(str(address), '에서 접속되었습니다.')
 
 while True :
-    send_data = input('>>>')
-    connection_socket.send(send_data.encode('utf-8'))
+    send_data = input('>>>')                            # 3. '>>>'의 출력과 함께 콘솔에서 입력을 받는다.
+    connection_socket.send(send_data.encode('utf-8'))   # 4. 입력받은 값을 encode해서 소켓에 전달한다.
 
-    received_data = connection_socket.recv(1024) # 1024 = 바이트 크기
-    print('상대방 메시지: ', received_data.decode('utf-8'))
+    received_data = connection_socket.recv(1024)        # 5. 연결된 소켓에서 받은 값을 decode해서 저장한다.
+    print('상대방 메시지: ', received_data.decode('utf-8'))# 6. print하고 3번을 반복한다.
 
